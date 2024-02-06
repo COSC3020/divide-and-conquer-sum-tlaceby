01fb71a1022c23423f63595ee6d1caca3d59da4b
tlaceby <tylerbadgertutorials@gmail.com>
2024-02-06 13:58:16 -0700

    Implimented Method

--- code.js
+++ code.js
@@ -1,3 +1,19 @@
+/**
+ * Recursive implimentation of sum where there are 3 splits.
+ * @param {number[]} a 
+ * @returns {number}
+ */
 function divideAndConquerSum(a) {
-    return a;
+    if (a.length === 0) return 0;
+    if (a.length === 1) return a[0];
+    if (a.length === 2) return a[0] + a[1];
+    
+    const splitsIndex = Math.floor(a.length / 3);
+    const left = a.slice(0, splitsIndex);
+    const middle = a.slice(splitsIndex, splitsIndex * 2);
+    const right = a.slice(splitsIndex * 2, a.length);
+
+    return divideAndConquerSum(left) 
+         + divideAndConquerSum(middle) 
+         + divideAndConquerSum(right);
 }
